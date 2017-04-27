6c4f2de57f1784abb6f5a1e98d5ea508926ce763
Chammavanijakul <ICHAMMA1@zebra.com>
2017-04-27 08:52:28 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: '90bd11e0-152b-472e-8da4-a521605b3a63', appPassword: 'ENyncF8svuaVVaYDzDxRgSE' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
