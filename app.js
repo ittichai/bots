aec3e4f1c5016c3f0771e55b2cab5537d24dd982
Chammavanijakul <ICHAMMA1@zebra.com>
2017-04-27 10:07:01 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
-// Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+  
 // Create chat bot
-var connector = new builder.ChatConnector
-({ appId: '90bd11e0-152b-472e-8da4-a521605b3a63', appPassword: 'ENyncF8svuaVVaYDzDxRgSE' }); 
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Create bot dialogs
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
-
-// Next
