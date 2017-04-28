f551c9f05d9c306ee4d035a772a2394581978324
Chammavanijakul <ICHAMMA1@zebra.com>
2017-04-28 09:13:48 -0500

    Updated api.js

---
+++ api.js
@@ -0,0 +1,25 @@
+   exports.GetSets = function (user, callback) {
+        request.get({
+            uri: 'https://api.quizlet.com/2.0/users/' + user + '/sets?client_id=<ENTER CLIENT ID here>',
+
+        },
+            function (error, response, body) {
+                if (error)
+                    callback(error);
+                else {
+                    body = JSON.parse(body);
+                    for (var x = 0; x < body.length; x++) {
+                        if ((x + 1) == body.length) {
+                            // last set
+                            sets = sets + body[x].title;
+                        } else {
+                            sets = sets + body[x].title + ', ';
+                        }
+                        table[body[x].title] = body[x].id; //creating a hash table to store set names and IDs
+                    }
+                    console.log('Got sets');
+                    exports.Sets = sets;
+
+                }
+            })
+    }
--- app.js
+++ app.js
@@ -10,17 +10,81 @@ server.listen(process.env.PORT || 3978, function()
 });
 
 // Create chat bot
+//var connector = new builder.ChatConnector
+//({ appId: process.env.my_app_id, appPassword: process.env.my_app_secret }); 
 var connector = new builder.ChatConnector
-({ appId: process.env.my_app_id, appPassword: process.env.my_app_secret }); 
+({ appId: "90bd11e0-152b-472e-8da4-a521605b3a63", appPassword: "ENyncF8svuaVVaYDzDxRgSE" }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Create bot dialogs
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+//server.get('/', restify.serveStatic({
+// directory: __dirname,
+// default: '/index.html'
+//}));
 
-server.get('/', restify.serveStatic({
- directory: __dirname,
- default: '/index.html'
-}));
+// Create bot dialogs
+
+bot.dialog('/',
+    function (session) {
+        session.send("Hello! Welcome to the Mhacks Quiz Bot. Would you like to study today?")
+        session.beginDialog('/user');
+    });
+
+bot.dialog('/user', new builder.IntentDialog()
+    .matches(/^yes/i, [
+        function (session) {
+        // setTimeout(function () {
+        if (username)
+            session.beginDialog('/subject')
+        else {
+            builder.Prompts.text(session, "What is your quizlet username?")
+        }
+        //  }, 3000)
+    },
+    function (session, results) {
+        quiz.GetSets(results.response);
+        session.beginDialog('/subject')
+    }])
+    .matches(/^no/i, function(session){
+        session.send("Ok see ya later!")
+        session.endConversation;
+    }));
+
+
+bot.dialog('/subject', [
+        function (session) {
+            setTimeout(function(){
+            builder.Prompts.text(session, "What study set would you like today?" + quiz.Sets);
+            }, 2000)
+        },
+        function (session, results) {
+            quiz.GetTerms(results.response);
+            session.send("Ok! I got your flashcards! Send 'ready' to begin. Send 'flip' for definition. Send 'next' for the next card. Send 'exit' when you are done")
+            session.beginDialog('/study')
+        }]
+);
+
+bot.dialog('/study', new builder.IntentDialog()
+    .matches(/^ready/i, [
+        function (session) {
+            session.send(quiz.Terms[index])
+        }])
+    .matches(/^flip/i, [
+        function (session) {
+            session.send(quiz.Def[index])
+        }]
+    )
+    .matches(/^next/i, [
+        function (session) {
+            if (++index == quiz.Terms.length)
+                session.send("You are all out of cards! Hope you had fun studying! :)")
+            else
+                session.send(quiz.Terms[index])
+        }])
+        .matches(/^exit/i, [
+        function (session) {
+            session.send("Hope you had fun studying. See ya later :)")
+        }]
+    )
+
+);
